32f3b45bfa2eae204b4ed11e6f891cbf869f5c5a
League Student <league@iMac-12.attlocal.net>
2019-12-31 14:55:41 -0800

    Learned how to do more advance shapes: size, color

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -1,34 +1,51 @@
 package section2;
 
+import java.awt.Color;
+
 import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+	Robot billyJeff = new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+billyJeff.setSpeed(100);
 		// 5. Set the pen width to 5
-
+billyJeff.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
 
 			// 7. Set the pen color to random
-	
+	billyJeff.penDown();
+	billyJeff.setPenColor(new Color(42,250,69));
 			// 1. Call the drawSquare() method
+	drawSquare();
 	
 			// 8. Turn the robot 90 degrees to the right
-
+billyJeff.turn(90);
 	}
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
+		JOptionPane.showMessageDialog(null, "sQuArE");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		for(int i=0;i<4;i++) {
+		billyJeff.move(250);
+		billyJeff.turn(90);
+		}
+		for(int i=0;i<4;i++) {
+			billyJeff.move(150);
+			billyJeff.turn(90);
+			billyJeff.setPenColor(new Color(42,0,250));
+		}
+		for(int i=0;i<4;i++) {
+			billyJeff.setPenColor(new Color(250,0,69));
+			billyJeff.move(50);
+			billyJeff.turn(90);
+			}
 	}
+	
 
 	public static void main(String[] args) {
 		new FourSquare().go();
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -1,25 +1,29 @@
 package section2;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot Jeff = new Robot();
 		// 5. Set your robot's pen down 
-		
+		Jeff.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+	Jeff.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for(int i=0;i<42069;i++) {
+			
 			// 7. Change the pen color to random
-		
+		Jeff.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+		Jeff.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		Jeff.turn(360/5);
 			// 8. Set the pen width to i
-			
+			Jeff.setPenWidth(10*i);
 	}
+}
 }
