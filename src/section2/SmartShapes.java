78331e3702120383ab3642bd96d7cbf1262095e6
League Student <league@iMac-12.attlocal.net>
2020-01-03 14:58:42 -0800

    Created a maze.

--- src/section2/SmartShapes.java
+++ src/section2/SmartShapes.java
@@ -6,24 +6,41 @@ public class SmartShapes {
 	
     public static void main(String[] args) throws Exception {
         // 1. Make a new Robot
-
+Robot robot = new Robot();
 
         // 3. Put the robot's pen down
 
-
+robot.penDown();
         // 6. Make the robot move as fast as possible
-
+robot.setSpeed(100);
 
         // 5. Use a for loop to repeat everything below 4 times. 
+for(int i=0;i<1;i++) {
 
+        //         
+	for(int r=0;r<40;r++) {
+	robot.move(10);
 
-        //         2. Move your robot 200 pixels
+       
 
+    	robot.turn(9);
+}
+	for(int t=0;t<40;t++) {
+		{
+			robot.move(10);
 
-        //         4. Turn the robot 90 degrees to the right
+		       
 
-    	
+		    	robot.turn(-9);
+		}
+	}
+		robot.move(-300);
+		robot.turn(-90);
+		robot.move(-30);
+		robot.turn(-90);
+		robot.move(-300);
     		// 7. Change steps 4 and 5 to draw a different shape (e.g. triangle (3-sides), pentagon (5-sides), decagon (10-sides)).
    
     }
+}
 }
